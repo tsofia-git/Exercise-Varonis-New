cecc255d3d48033a9e5f09558702dfe2c62c87c0
TsofiaCohen <tsofia.c@cybord.ai>
2024-02-14 22:46:26 +0200

    push code

--- python code.py
+++ python code.py
@@ -1,1 +1,24 @@
 from github import Github
+import os
+# from dotenv import load_dotenv
+#
+# load_dotenv(ENV_PATH)
+token = "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" #os.getenv('GITHUB_TOKEN')
+if not token:
+    print("Error: Personal Access Token not found. Please set GITHUB_TOKEN environment variable.")
+    exit()
+
+# Create a PyGithub instance using the Personal Access Token
+g = Github(token)
+
+# Specify the repository to check
+repo_name = "tsofia-git/Exercise-Varonis-New"  # Replace with the repository name in the format "owner/repository"
+repo = g.get_repo(repo_name)
+
+# Check if the repository is public
+if not repo.private:
+    print(f"The repository '{repo_name}' is currently PUBLIC. Changing to PRIVATE...")
+    repo.edit(private=True)
+    print(f"The repository '{repo_name}' has been successfully changed to PRIVATE.")
+else:
+    print(f"The repository '{repo_name}' is already PRIVATE.")
